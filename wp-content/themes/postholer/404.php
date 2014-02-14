d6af6901282cda62ff3af39411e320bd248d2288
Wade Dorrell <wade@dorrells.org>
2014-02-13 22:41:11 -0700

    Typo

--- wp-content/themes/postholer/404.php
+++ wp-content/themes/postholer/404.php
@@ -1,3 +1,3 @@
 <?php get_header(); ?>
-+709. Just kidding. 404. Perhaps you want the <a href="/feed/">feed?</a>
+709. Just kidding. 404. Perhaps you want the <a href="/feed/">feed?</a>
 <?php get_footer(); ?>
