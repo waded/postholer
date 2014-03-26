c5c805d19ead3857093aad5ed30245b414796a1a
Wade Dorrell <wade@dorrells.org>
2014-03-26 01:58:16 -0600

    Style, text, and typo fixes

--- wp-content/themes/postholer/index.php
+++ wp-content/themes/postholer/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <h1>Index</h1>
-<div class="contentish">This theme currently doesn't have an index page. The mobile device 
-	should use the <a href="/feed/">feed</a> to determine which posts
-	to show</div>
+<div class="contentish">This theme currently doesn't have an index page. The beacon app 
+	should use the <a href="/feed/">RSS2 feed</a> or <a href="/atom/">Atom feed</a> 
+	to determine which post URLs to use with which beacon UUID &amp; major/minor numbers.</div>
 <?php get_footer(); ?>
