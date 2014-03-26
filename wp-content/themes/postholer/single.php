2b3b43e03b446926bdcd0106be41ece0dd80edd9
Wade Dorrell <wade@dorrells.org>
2014-03-26 02:02:21 -0600

    Style

--- wp-content/themes/postholer/single.php
+++ wp-content/themes/postholer/single.php
@@ -2,7 +2,7 @@
 get_header();
 while ( have_posts() ) : the_post(); ?>
 	<h1><? the_title() ?></h1>
-	<div><? the_content('') ?></div>	
+	<div class="contentish"><? the_content('') ?></div>	
 <? 
 endwhile;
 get_footer();
