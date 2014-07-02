7a5bb62af482f79708dc9546c647e17961bd0338
Wade Dorrell <wade@dorrells.org>
2014-07-02 00:18:38 -0600

    Better code through not typing in the wrong file

--- wp-content/themes/postholer/single.php
+++ wp-content/themes/postholer/single.php
@@ -1,7 +1,9 @@
-<?php get_header(); ?>
+<?php
+get_header();
 while ( have_posts() ) : the_post(); ?>
 	<h2><? the_title() ?></h2>
 	<div><? the_content('') ?></div>	
 <? 
 endwhile;
-<?php get_footer(); ?>
+get_footer();
+?>
