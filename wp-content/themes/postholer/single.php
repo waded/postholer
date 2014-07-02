5ce54717a69d07adb81a210b9d43e20032f05573
Wade Dorrell <wade@dorrells.org>
2014-07-02 00:15:16 -0600

    Improvement over last commit

--- wp-content/themes/postholer/single.php
+++ wp-content/themes/postholer/single.php
@@ -1,9 +1,7 @@
-<?php
-get_header();
+<?php get_header(); ?>
 while ( have_posts() ) : the_post(); ?>
 	<h2><? the_title() ?></h2>
 	<div><? the_content('') ?></div>	
 <? 
 endwhile;
-get_footer();
-?>
+<?php get_footer(); ?>
