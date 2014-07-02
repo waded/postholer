0a96936da02c7411b2cdbceb570e10d0963f52b9
Wade Dorrell <wade@dorrells.org>
2014-07-02 00:23:05 -0600

    Added next post link on index

--- wp-content/themes/postholer/single.php
+++ wp-content/themes/postholer/single.php
@@ -3,7 +3,14 @@ get_header();
 while ( have_posts() ) : the_post(); ?>
 	<h2><? the_title() ?></h2>
 	<div><? the_content('') ?></div>	
-<? 
+
+	<?php $next_post = get_next_post();
+	if (!empty( $next_post )): ?>
+  		<a href="<?php echo get_permalink($next_post->ID);?>">
+  			<?php echo $next_post->post_title;?>
+  		</a>
+	<?php endif;
+
 endwhile;
 get_footer();
 ?>
