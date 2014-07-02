e8c292dc9ce0eee5e75e92fb7d5dc7cf03982010
Wade Dorrell <wade@dorrells.org>
2014-07-02 00:30:04 -0600

    ~~

--- wp-content/themes/postholer/index.php
+++ wp-content/themes/postholer/index.php
@@ -3,7 +3,7 @@ get_header();
 while ( have_posts() ) : the_post(); ?>
 	<h2><? the_title() ?></h2>
 	<div><? the_content('') ?></div>	
-	<div><?php previous_post_link('Previous: %link'); ?></div>
+	<div><? previous_post_link('Previous: %link'); ?></div>
 <? 
 endwhile;
 get_footer();
--- wp-content/themes/postholer/single.php
+++ wp-content/themes/postholer/single.php
@@ -2,7 +2,8 @@
 get_header();
 while ( have_posts() ) : the_post(); ?>
 	<h2><? the_title() ?></h2>
-	<div><? the_content('') ?></div>
+	<div><? the_content('') ?></div>	
+	<div><? previous_post_link('Previous: %link'); ?></div>
 <? 
 endwhile;
 get_footer();
