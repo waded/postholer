20cc1bd4039f38d843d63d5aa52a0dca2da94d09
Wade Dorrell <wade@dorrells.org>
2014-07-03 22:17:04 -0600

    Georgia on my link rel

--- wp-content/themes/postholer/header.php
+++ wp-content/themes/postholer/header.php
@@ -5,7 +5,7 @@
 	<title><?php wp_title(''); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="<?php get_stylesheet_uri(); ?>">
 	<?php wp_head(); ?>
 </head>
 <body>
