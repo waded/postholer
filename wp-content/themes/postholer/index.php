f2c092f7ed93c0c3b37e5ca0cdb4356079c75075
Wade Dorrell <wade@dorrells.org>
2014-08-03 18:11:05 -0600

    Added byline

--- wp-content/themes/postholer/index.php
+++ wp-content/themes/postholer/index.php
@@ -3,7 +3,11 @@ get_header();
 while ( have_posts() ) : the_post(); ?>
 	<div class="post index">
 		<h2><? the_title() ?></h2>
-		<div class="content"><? the_content('') ?></div>	
+		<div class="byline">
+			<div class="author"><? the_author() ?></div>
+			<div class="date"><? the_date() ?></div>
+		</div>
+		<div class="content"><? the_content('') ?></div>
 		<div class="previous"><? previous_post_link('Previous: %link'); ?></div>
 	</div>
 <? 
